cf0829b9080f7feeacce3572515c03df70af7c73
Yaroslav Leskovskiy <yaroslavlesk@gmail.com>
2020-07-30 00:28:13 +0300

    method and options

---
+++ method.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const str = 'text';
+const arr = [1, 2, 3];
+
+console.log(str.length);
+console.log(arr.length);
+
+
+const str_1 = 'text';
+console.log(str_1.toLocaleUpperCase());
+
+
+const str_2 = 'text';
+
+console.log(str_2.slice(1, 3));
+console.log(str_2.substring(1, 3));
+console.log(str_2.substr(1, 2));
+
+
+const num = 12.2;
+console.log(Math.round(num));
+
+
+const pix = "12.2px";
+console.log(parseInt(pix));
+console.log(parseFloat(pix));
